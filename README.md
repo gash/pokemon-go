0b7942a701fb068a45e61ca17473dd2380142d07
Samuel Friedman <SamuelFriedman@users.noreply.github.com>
2020-05-17 06:47:44 -0700

    Basic functionality to create pokemon-go. Translates from pokemon-go to go.

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # pokemon-go
 2020.85: your programming language that is just a crappy wrapper for Go, where all the keywords are now Pokémon
+
+## Running pokemon-go
+
+You will need a Go compiler. To create the compiler, execute the command:
+```
+go build -o pokemon_go pokemon_go.go
+```
+
+To run the code:
+```
+./pokemon_go pokemon_go_example.pgo
+go run temp.go
+```
+
+
---
+++ pokemon_go.go
@@ -0,0 +1,468 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+	"text/scanner"
+	"os"
+	"os/exec"
+	"io/ioutil"
+)
+
+// Copyright 2009 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package token defines constants representing the lexical tokens of the Go
+// programming language and basic operations on tokens (printing, predicates).
+//
+
+import (
+	"strconv"
+	"unicode"
+	"unicode/utf8"
+)
+
+// Token is the set of lexical tokens of the Go programming language.
+type Token int
+
+// The list of tokens.
+const (
+	// Special tokens
+	ILLEGAL Token = iota
+	EOF
+	COMMENT
+
+	literal_beg
+	// Identifiers and basic type literals
+	// (these tokens stand for classes of literals)
+	IDENT  // main
+	INT    // 12345
+	FLOAT  // 123.45
+	IMAG   // 123.45i
+	CHAR   // 'a'
+	STRING // "abc"
+	literal_end
+
+	operator_beg
+	// Operators and delimiters
+	ADD // +
+	SUB // -
+	MUL // *
+	QUO // /
+	REM // %
+
+	AND     // &
+	OR      // |
+	XOR     // ^
+	SHL     // <<
+	SHR     // >>
+	AND_NOT // &^
+
+	ADD_ASSIGN // +=
+	SUB_ASSIGN // -=
+	MUL_ASSIGN // *=
+	QUO_ASSIGN // /=
+	REM_ASSIGN // %=
+
+	AND_ASSIGN     // &=
+	OR_ASSIGN      // |=
+	XOR_ASSIGN     // ^=
+	SHL_ASSIGN     // <<=
+	SHR_ASSIGN     // >>=
+	AND_NOT_ASSIGN // &^=
+
+	LAND  // &&
+	LOR   // ||
+	ARROW // <-
+	INC   // ++
+	DEC   // --
+
+	EQL    // ==
+	LSS    // <
+	GTR    // >
+	ASSIGN // =
+	NOT    // !
+
+	NEQ      // !=
+	LEQ      // <=
+	GEQ      // >=
+	DEFINE   // :=
+	ELLIPSIS // ...
+
+	LPAREN // (
+	LBRACK // [
+	LBRACE // {
+	COMMA  // ,
+	PERIOD // .
+
+	RPAREN    // )
+	RBRACK    // ]
+	RBRACE    // }
+	SEMICOLON // ;
+	COLON     // :
+	operator_end
+
+	keyword_beg
+	// Keywords
+	BREAK
+	CASE
+	CHAN
+	CONST
+	CONTINUE
+
+	DEFAULT
+	DEFER
+	ELSE
+	FALLTHROUGH
+	FOR
+
+	FUNC
+	GO
+	GOTO
+	IF
+	IMPORT
+	IMPORTGO
+
+	INTERFACE
+	MAP
+	PACKAGE
+	RANGE
+	RETURN
+
+	SELECT
+	STRUCT
+	SWITCH
+	TYPE
+	VAR
+	keyword_end
+)
+
+var tokens = [...]string{
+	ILLEGAL: "ILLEGAL",
+
+	EOF:     "EOF",
+	COMMENT: "COMMENT",
+
+	IDENT:  "IDENT",
+	INT:    "INT",
+	FLOAT:  "FLOAT",
+	IMAG:   "IMAG",
+	CHAR:   "CHAR",
+	STRING: "STRING",
+
+	ADD: "+",
+	SUB: "-",
+	MUL: "*",
+	QUO: "/",
+	REM: "%",
+
+	AND:     "&",
+	OR:      "|",
+	XOR:     "^",
+	SHL:     "<<",
+	SHR:     ">>",
+	AND_NOT: "&^",
+
+	ADD_ASSIGN: "+=",
+	SUB_ASSIGN: "-=",
+	MUL_ASSIGN: "*=",
+	QUO_ASSIGN: "/=",
+	REM_ASSIGN: "%=",
+
+	AND_ASSIGN:     "&=",
+	OR_ASSIGN:      "|=",
+	XOR_ASSIGN:     "^=",
+	SHL_ASSIGN:     "<<=",
+	SHR_ASSIGN:     ">>=",
+	AND_NOT_ASSIGN: "&^=",
+
+	LAND:  "&&",
+	LOR:   "||",
+	ARROW: "<-",
+	INC:   "++",
+	DEC:   "--",
+
+	EQL:    "==",
+	LSS:    "<",
+	GTR:    ">",
+	ASSIGN: "=",
+	NOT:    "!",
+
+	NEQ:      "!=",
+	LEQ:      "<=",
+	GEQ:      ">=",
+	DEFINE:   ":=",
+	ELLIPSIS: "...",
+
+	LPAREN: "(",
+	LBRACK: "[",
+	LBRACE: "{",
+	COMMA:  ",",
+	PERIOD: ".",
+
+	RPAREN:    ")",
+	RBRACK:    "]",
+	RBRACE:    "}",
+	SEMICOLON: ";",
+	COLON:     ":",
+
+	BREAK:    "meowth",
+	CASE:     "jigglypuff",
+	CHAN:     "gyarados",
+	CONST:    "pidgey",
+	CONTINUE: "psyduck",
+
+	DEFAULT:     "wooloo",
+	DEFER:       "yamper",
+	ELSE:        "eevee",
+	FALLTHROUGH: "bulbasaur",
+	FOR:         "squirtle",
+
+	FUNC:   "mudkip",
+	GO:     "lucario",
+	GOTO:   "snorlax",
+	IF:     "mew",
+	IMPORT: "charmander",
+	IMPORTGO: "drifloon",
+
+	INTERFACE: "alolanexeggcutor",
+	MAP:       "togepi",
+	PACKAGE:   "bidoof",
+	RANGE:     "furret",
+	RETURN:    "lillipup",
+
+	SELECT: "buneary",
+	STRUCT: "lickitung",
+	SWITCH: "braviary",
+	TYPE:   "charizard",
+	VAR:    "pikachu",
+}
+
+// String returns the string corresponding to the token tok.
+// For operators, delimiters, and keywords the string is the actual
+// token character sequence (e.g., for the token ADD, the string is
+// "+"). For all other tokens the string corresponds to the token
+// constant name (e.g. for the token IDENT, the string is "IDENT").
+//
+func (tok Token) String() string {
+	s := ""
+	if 0 <= tok && tok < Token(len(tokens)) {
+		s = tokens[tok]
+	}
+	if s == "" {
+		s = "token(" + strconv.Itoa(int(tok)) + ")"
+	}
+	return s
+}
+
+// A set of constants for precedence-based expression parsing.
+// Non-operators have lowest precedence, followed by operators
+// starting with precedence 1 up to unary operators. The highest
+// precedence serves as "catch-all" precedence for selector,
+// indexing, and other operator and delimiter tokens.
+//
+const (
+	LowestPrec  = 0 // non-operators
+	UnaryPrec   = 6
+	HighestPrec = 7
+)
+
+// Precedence returns the operator precedence of the binary
+// operator op. If op is not a binary operator, the result
+// is LowestPrecedence.
+//
+func (op Token) Precedence() int {
+	switch op {
+	case LOR:
+		return 1
+	case LAND:
+		return 2
+	case EQL, NEQ, LSS, LEQ, GTR, GEQ:
+		return 3
+	case ADD, SUB, OR, XOR:
+		return 4
+	case MUL, QUO, REM, SHL, SHR, AND, AND_NOT:
+		return 5
+	}
+	return LowestPrec
+}
+
+var keywords map[string]Token
+
+func init() {
+	keywords = make(map[string]Token)
+	for i := keyword_beg + 1; i < keyword_end; i++ {
+		keywords[tokens[i]] = i
+	}
+}
+
+// Lookup maps an identifier to its keyword token or IDENT (if not a keyword).
+//
+func Lookup(ident string) Token {
+	if tok, is_keyword := keywords[ident]; is_keyword {
+		return tok
+	}
+	return IDENT
+}
+
+// Predicates
+
+// IsLiteral returns true for tokens corresponding to identifiers
+// and basic type literals; it returns false otherwise.
+//
+func (tok Token) IsLiteral() bool { return literal_beg < tok && tok < literal_end }
+
+// IsOperator returns true for tokens corresponding to operators and
+// delimiters; it returns false otherwise.
+//
+func (tok Token) IsOperator() bool { return operator_beg < tok && tok < operator_end }
+
+// IsKeyword returns true for tokens corresponding to keywords;
+// it returns false otherwise.
+//
+func (tok Token) IsKeyword() bool { return keyword_beg < tok && tok < keyword_end }
+
+// IsExported reports whether name starts with an upper-case letter.
+//
+func IsExported(name string) bool {
+	ch, _ := utf8.DecodeRuneInString(name)
+	return unicode.IsUpper(ch)
+}
+
+// IsKeyword reports whether name is a Go keyword, such as "func" or "return".
+//
+func IsKeyword(name string) bool {
+	// TODO: opt: use a perfect hash function instead of a global map.
+	_, ok := keywords[name]
+	return ok
+}
+
+// IsIdentifier reports whether name is a Go identifier, that is, a non-empty
+// string made up of letters, digits, and underscores, where the first character
+// is not a digit. Keywords are not identifiers.
+//
+func IsIdentifier(name string) bool {
+	for i, c := range name {
+		if !unicode.IsLetter(c) && c != '_' && (i == 0 || !unicode.IsDigit(c)) {
+			return false
+		}
+	}
+	return name != "" && !IsKeyword(name)
+}
+
+/* }
+
+// This is scanned code.
+charizard a > 10 {
+// if this is true
+	someParsable = text
+*/
+
+func main() {
+/*	src = `
+bidoof main
+
+charmander "fmt"
+
+mudkip main() {
+
+    mew 7 % 2 == 0 {
+        fmt.Println("7 is even")
+    } else {
+        fmt.Println("7 is odd")
+    }
+
+    mew 8 % 4 == 0 {
+        fmt.Println("8 is divisible by 4")
+    }
+
+    mew num := 9; num < 0 {
+        fmt.Println(num, "is negative")
+    } else mew num < 10 {
+        fmt.Println(num, "has 1 digit")
+    } else {
+        fmt.Println(num, "has multiple digits")
+    }
+}`*/
+
+	var s scanner.Scanner
+	//var src string
+	src, _ := ioutil.ReadFile(os.Args[1])
+	s.Init(strings.NewReader(string(src)))
+	s.Filename = "example"
+	s.Whitespace = 0 // Include all whitespace
+	m := make(map[string]string)
+	m["pikachu"] = "var"
+	m["meowth"] = "break"
+	m["jigglypuff"] = "case"
+	m["gyarados"] = "chan"
+	m["eevee"] = "else"
+	m["pidgey"] = "const"
+	m["psyduck"] = "continue"
+	m["wooloo"] = "default"
+	m["yamper"] = "defer"
+	m["bulbasaur"] = "fallthrough"
+	m["squirtle"] = "for"
+	m["charmander"] = "import"
+	m["mudkip"] = "func"
+	m["lucario"] = "go"
+	m["snorlax"] = "goto"
+	m["alolanexeggcutor"] = "interface"
+	m["drifloon"] = "importgo"
+	m["mew"] = "if"
+	m["togepi"] = "map"
+	m["bidoof"] = "package"
+	m["furret"] = "range"
+	m["lillipup"] = "return"
+	m["buneary"] = "select"
+	m["lickitung"] = "struct"
+	m["braviary"] = "switch"
+	m["charizard"] = "type"
+	/*
+	m["var"] = "pikachu"
+	m["break"] = "meowth"
+	m["case"] = "jigglypuff"
+	m["chan"] = "gyarados"
+	m["else"] = "eevee"
+	m["const"] = "pidgey"
+	m["continue"] = "psyduck"
+	m["default"] = "wooloo"
+	m["defer"] = "yamper"
+	m["fallthrough"] = "bulbasaur"
+	m["for"] = "squirtle"
+	m["import"] = "charmander"
+	m["func"] = "mudkip"
+	m["go"] = "lucario"
+	m["goto"] = "snorlax"
+	m["interface"] = "alolanexeggcutor"
+	m["importgo"] = "drifloon"
+	m["if"] = "mew"
+	m["map"] = "togepi"
+	m["package"] = "bidoof"
+	m["range"] = "furret"
+	m["return"] = "lillipup"
+	m["select"] = "buneary"
+	m["struct"] = "lickitung"
+	m["switch"] = "braviary"
+	m["type"] = "charizard"
+	*/
+	//s.Mode =
+	var str strings.Builder
+	for tok := s.Scan(); tok != scanner.EOF; tok = s.Scan() {
+		word := s.TokenText()
+		if IsKeyword(s.TokenText()) {
+			word = m[s.TokenText()]
+		}
+		//fmt.Printf("%s: %s %t\n", s.Position, word, IsKeyword(s.TokenText()))
+		pprint_word := fmt.Sprintf("%s",word)
+		//fmt.Printf("%s",pprint_word)
+		str.WriteString(pprint_word)
+		//str.WriteString(fmt.Sprintf("%s",word))
+	}
+	//fmt.Printf(str.String())
+	f, _ := os.Create("temp.go")
+	f.WriteString(str.String())
+	f.Close()
+	exec.Command("go run","temp.go")
+}
